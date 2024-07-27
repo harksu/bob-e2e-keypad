bbd1d6a8ea0f1619e1e67e4799e0f32e69b84798
harksu <harksulim@naver.com>
2024-07-27 15:07:48 +0900

    feat: return image json

--- src/main/kotlin/bob/simple/spring/controller/TestController.kt
+++ src/main/kotlin/bob/simple/spring/controller/TestController.kt
@@ -2,14 +2,27 @@ package bob.simple.spring.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import java.util.Base64
+import java.nio.file.Files
+import java.nio.file.Paths
 
-data class MessageResponse(val message: String, val author: String)
+data class MessageResponse(val message: String, val author: String,val image: String?)
 
 @RestController
+@RequestMapping("/api")
 class TestController {
-    @GetMapping("/e2e")
 
-    fun getE2E(): MessageResponse {
-        return MessageResponse(message = "this is keypad", author = "hi" )
+    @GetMapping("/message")
+    fun getMessage(): MessageResponse {
+        val imagePath = Paths.get("src/main/resources/static/sample-image.png")
+        val imageBytes = Files.readAllBytes(imagePath)
+        val encodedImage = Base64.getEncoder().encodeToString(imageBytes)
+
+        return MessageResponse(
+            message = "this is test-template for e2e keypad",
+            author = "harksulim",
+            image = encodedImage
+        )
     }
 }
