587716e50b567aab1f9e0cd53b16c1bec5f563fb
harksu <harksulim@naver.com>
2024-07-27 14:55:24 +0900

    chore: keypad_image

--- src/main/kotlin/bob/simple/spring/controller/TestController.kt
+++ src/main/kotlin/bob/simple/spring/controller/TestController.kt
@@ -3,10 +3,13 @@ package bob.simple.spring.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+data class MessageResponse(val message: String, val author: String)
+
 @RestController
 class TestController {
-    @GetMapping("/test/a")
-    fun a(): String {
-        return "Hello Spring"
+    @GetMapping("/e2e")
+
+    fun getE2E(): MessageResponse {
+        return MessageResponse(message = "this is keypad", author = "hi" )
     }
 }
